83000e3786fef7202e22e07c75225787aa87f251
Ultron Patil <ultronpatil@gmail.com>
2024-05-30 04:58:42 +0530

    car slot color changes to red

--- frontend/src/My_componants/Car.js
+++ frontend/src/My_componants/Car.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './Car.css';
 import axios from 'axios';
 
-const Car = ({ slotNumber, onSlotSelect, isSelected, state }) => {
+const Car = ({ slotNumber, onSlotSelect, isSelected, state, userEmail }) => {
     const [isBooked, setIsBooked] = useState(false);
-    const [isOccupied, setIsOccupied] = useState(false);
+    const [isOccupied, setIsOccupied] = useState(state);
 
     useEffect(() => {
         const fetchReservationStatus = async () => {
             try {
                 const response = await axios.get(`http://localhost:3002/checkSlotStatus/${slotNumber}`);
-                const { booked, occupied } = response.data;
+                const { booked } = response.data;
                 setIsBooked(booked);
-                setIsOccupied(occupied);
             } catch (error) {
                 console.error('Error fetching slot status:', error);
             }
@@ -20,6 +19,32 @@ const Car = ({ slotNumber, onSlotSelect, isSelected, state }) => {
 
         fetchReservationStatus();
     }, [slotNumber]);
+
+    useEffect(() => {
+        setIsOccupied(state);
+    }, [state]);
+
+
+    const handleOccupancyChange = (isOccupied) => {
+        // Check if the slot is occupied and booked by the current user
+        if (isOccupied && isBooked && userEmail) {
+            generateAlertForUser();
+        }
+    };
+
+    const generateAlertForUser = () => {
+        const isUser = window.confirm("Someone is parking on your spot, is this you?\n1. Yes\n2. No");
+        if (isUser) {
+            alert("Thanks for parking");
+        } else {
+            alert("Alarm has been raised");
+        }
+    };
+
+    useEffect(() => {
+        handleOccupancyChange(isOccupied);
+    }, [isOccupied, isBooked, userEmail]);
+
 
     const handleClick = () => {
         if (!isBooked && !isOccupied) {
